ef1305d772b5eef855e334068276d3a704d9e439
LimbersMay <limbermay1@gmail.com>
2023-09-27 14:58:03 -0600

    (languageFormat.ts) Documentación adicional del archivo agregado

--- src/languages/languageFormat.ts
+++ src/languages/languageFormat.ts
@@ -1,3 +1,8 @@
+/*
+    This file contains the format of the language files.
+
+    The purpose of this file is to make sure that all language files have the same format with the same properties.
+ */
 
 export interface LanguageFormat {
     title: string;
