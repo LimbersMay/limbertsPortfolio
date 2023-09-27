6fc1c658c1adbb856f237d9daa35fb3840384bff
LimbersMay <limbermay1@gmail.com>
2023-09-27 13:43:51 -0600

    (src main) Provider con el store de redux configurado

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import PortfolioApp from './PortfolioApp.tsx'
+import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 import './index.css'
+import PortfolioApp from './PortfolioApp.tsx'
+import {store} from "./store";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
         <BrowserRouter>
-            <PortfolioApp/>
+            <Provider store={store}>
+                <PortfolioApp/>
+            </Provider>
         </BrowserRouter>
     </React.StrictMode>,
 )
