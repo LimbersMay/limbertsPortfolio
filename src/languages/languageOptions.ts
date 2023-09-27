09e05c473bfbb563eacd9f09357d179888726fd4
LimbersMay <limbermay1@gmail.com>
2023-09-27 14:58:17 -0600

    (languages languageOptions) Documentación adicional del archivo agregado

--- src/languages/languageOptions.ts
+++ src/languages/languageOptions.ts
@@ -1,5 +1,9 @@
 import {en, es } from "./";
 
+/*
+    This file is used to export all the languages that are available in the application.
+ */
+
 export interface Dictionary {
     EN: typeof en;
     ES: typeof es;
