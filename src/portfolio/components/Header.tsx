e8f69d54d4afdc4a7a575d17b74a155d3ba685fc
LimbersMay <limbermay1@gmail.com>
2023-10-02 08:39:56 -0600

    (components Header) Se manejó el cambio de idioma del usuario

--- src/portfolio/components/Header.tsx
+++ src/portfolio/components/Header.tsx
@@ -1,10 +1,20 @@
+import {ChangeEvent} from "react";
 import {Text} from "./";
+import {usePreferencesStore} from "../../hooks";
+import {Dictionary} from "../../languages";
 
 export const Header = () => {
+
+    const { changeLanguage, language } = usePreferencesStore();
+
+    const onChangeLanguage = ({ target }: ChangeEvent<HTMLSelectElement>) => {
+        changeLanguage(target.value as keyof Dictionary);
+    }
+
     return (
-        <header className="flex mb-10 mt-3 items-center">
+        <header className="mt-3 mb-10 flex justify-between">
             { /* Title */}
-            <p className="font-bold text-xl text-slate-800 hidden md:block">
+            <p className="text-xl font-bold text-slate-800 md:block">
                 LIMBERT MAY
             </p>
 
@@ -24,18 +34,24 @@ export const Header = () => {
                 </button>
 
                 { /* Language options */}
-                <div className="flex flex-row items-center gap-5 rounded border border-slate-400">
+                <div className="flex flex-row items-center gap-1 rounded border border-slate-400">
 
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-5 w-5">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 21l5.25-11.25L21 21m-9-3h7.5M3 5.621a48.474 48.474 0 016-.371m0 0c1.12 0 2.233.038 3.334.114M9 5.25V3m3.334 2.364C11.176 10.658 7.69 15.08 3 17.502m9.334-12.138c.896.061 1.785.147 2.666.257m-4.589 8.495a18.023 18.023 0 01-3.827-5.802" />
                     </svg>
 
-                    <select className="focus:outline-none appearance-none bg-inherit">
-                        <option value="en">English</option>
-                        <option value="es">Spanish</option>
+                    <select value={language} onChange={onChangeLanguage} className="appearance-none bg-inherit focus:outline-none">
+                        <option value="EN">English</option>
+                        <option value="ES">Spanish</option>
                     </select>
                 </div>
             </div>
+
+            <button className="sm:hidden">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-6 w-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                </svg>
+            </button>
         </header>
     )
 }
