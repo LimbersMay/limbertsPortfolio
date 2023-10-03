2c540766687f9725207d226c0daed9bdebed7dc3
LimbersMay <limbermay1@gmail.com>
2023-10-03 10:48:58 -0600

    (components ProjectsSection) Se agregó un ID al div para navegar a él desde el header y la descripción se obtuvo desde el componente Text

--- src/portfolio/components/ProjectsSection.tsx
+++ src/portfolio/components/ProjectsSection.tsx
@@ -8,13 +8,13 @@ interface Props {
 
 export const ProjectsSection = ({ projects }: Props) => {
     return (
-        <div className="mt-5 flex flex-col gap-5">
+        <div id="projects" className="mt-5 flex flex-col gap-5">
             <h1 className="text-3xl font-bold text-slate-900">
                 <Text tid="projectsTitle"/>
             </h1>
 
             <p className="text-slate-600">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium amet atque aut fugit iusto libero
+                <Text tid="projectsDescription"/>
             </p>
 
             { /* Project cards*/}
