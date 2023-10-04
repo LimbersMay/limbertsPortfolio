80696bc4dc7941b1f9e7623cd09b3adc85d6d071
LimbersMay <limbermay1@gmail.com>
2023-10-04 09:52:20 -0600

    (components ContactCard)  se formatearon las clases

--- src/portfolio/components/ContactCard.tsx
+++ src/portfolio/components/ContactCard.tsx
@@ -4,12 +4,12 @@ export const ContactCard = () => {
     return (
         <div className="flex flex-col gap-3 bg-gray-100 p-6 shadow-xl">
             <p className="text-slate-900">
-                <span className="text-slate-900 font-bold"><Text tid="contactName"/>:</span> Limbert Otoniel May Ek
+                <span className="font-bold text-slate-900"><Text tid="contactName"/>:</span> Limbert Otoniel May Ek
             </p>
             <p className="text-slate-900">
-                <span className="text-slate-900 font-bold"><Text tid="contactEmail"/>: </span>limbermay1@gmail.com
+                <span className="font-bold text-slate-900"><Text tid="contactEmail"/>: </span>limbermay1@gmail.com
             </p>
-            <a className="border border-solid p-2 self-start bg-slate-900 rounded text-slate-100"
+            <a className="self-start rounded border border-solid bg-slate-900 p-2 text-slate-100"
                href="mailto:limbermay1@gmail.com"><Text tid="contactSendEmail" /></a>
         </div>
     )
